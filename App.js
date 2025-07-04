0b77b6c899cf757b5403fd8f97a7a62798216208
Athmate09 <Athmateofficial@gmail.com>
2025-07-05 00:03:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
-// App component placeholder
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Athmate</h1>
+      <p>India’s Premium Athlete Wear</p>
+    </div>
+  );
+}
+
+export default App;// App component placeholder
